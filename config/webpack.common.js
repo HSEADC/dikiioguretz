91b179b6927a4b0fbb19e2de9ebef41e857087ad
alina m <alinamocalova451@gmail.com>
2025-09-30 11:14:53 +0300

    routing

---
+++ config/webpack.common.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const htmlPages = require('./webpack.pages')
+//const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+ entry: {
+  index: './src/js/index.js'
+ },
+output: {
+ path: path.resolve(__dirname, 'docs'),
+ filename: '[name].js',
+ },
+ module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        },
+      },
+
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|webp|gif)/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][quert]'
+        }
+      },
+
+      {
+        test: /\.(ttf|otf|woff\woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    ...htmlPages
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: "source", to: "dest" },
+    //     { from: "other", to: "public" },
+    //   ],
+    // }),
+  ],
+};
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+console.log('helllo');
+
+import '../stylesheets/style.css';
