3a39838a43a9ec1cab951e2fa608dd8e1102cbcb
elirudaya <rudayaelinaa@gmail.com>
2025-10-22 22:35:19 +0300

    plug

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const htmlPages = require('./webpack.pages')
 //const CopyPlugin = require("copy-webpack-plugin");
