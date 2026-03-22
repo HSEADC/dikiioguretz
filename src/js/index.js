338ae1722cd30261bb1ecb2276692ba2f3e35e43
elirudaya <rudayaelinaa@gmail.com>
2026-03-23 02:24:34 +0300

    articles final page

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
-console.log('hey')
 import '../stylesheets/style.css'
+
+var form = document.getElementById('my-form')
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  var status = document.getElementById('my-form-status')
+  var data = new FormData(event.target)
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = 'спасибо, оставайтесь с нами=)'
+        form.reset()
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data['errors']
+              .map((error) => error['message'])
+              .join(', ')
+          } else {
+            status.innerHTML = 'Упс.. Что-то пошло не так. Попробуйте еще раз=)'
+          }
+        })
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = 'Упс.. Что-то пошло не так. Попробуйте еще раз=)'
+    })
+}
+form.addEventListener('submit', handleSubmit)
