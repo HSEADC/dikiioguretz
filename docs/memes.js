cf83ba0ced7ee2ff0ff7c317f10cdcc7fa65c536
elirudaya <rudayaelinaa@gmail.com>
2026-03-23 22:43:23 +0300

    build meme js

--- docs/memes.js
+++ docs/memes.js
@@ -3837,75 +3837,62 @@ module.exports = Airtable;
 "use strict";
 /* harmony import */ var airtable__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
 /* harmony import */ var airtable__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(airtable__WEBPACK_IMPORTED_MODULE_0__);
+alert('JS работает!');
 
-
-// поменять токен перед пущем!!!!
-// const token =
-//   'patEG59q9n6nmP5cv.1c7f4c3308ec509316416a3206ffda0e300d653b9d39c0a03f00fa7d5c6369c1'
-
-// Airtable.configure({
-//   endpointUrl: 'https://api.airtable.com',
-//   apiKey: token
-// })
-
-// var base = Airtable.base('app0vbth4eKDLaq0B')
-
-// getMemes().then((content) => {
-//   updateMemes(content)
-// })
-
-// function getMemes() {
-//   return new Promise((resolve) => {
-//     const content = []
-//     base('C_DMemes')
-//       .select({ maxRecords: 50 })
-//       .firstPage()
-//       .then((result) => {
-//         result.forEach((record) => {
-//           content.push({
-//             id: record.id,
-//             image: record.fields['Images']
-//           })
-//         })
-//         resolve(content)
-//       })
-//   })
-// }
-
-// function updateMemes(content) {
-//   content.forEach((stroke) => {
-//     createMemeCard(stroke)
-//   })
-// }
-
-// function createMemeCard(stroke) {
-//   let { image } = stroke
-
-//   const card = document.createElement('div')
-//   card.classList.add('M_DMemesTeaser')
-
-//   const imageFrame = document.createElement('div')
-//   imageFrame.classList.add('M_DSitesTeaserImage')
-
-//   const imageInner = document.createElement('div')
-//   imageInner.classList.add('M_DSitesTeaserImageInner')
-
-//   let imageURl = ''
-//   if (Array.isArray(image) && image.length > 0) {
-//     imageURl = image[0].url
-//   } else if (typeof image === 'string') {
-//     imageURl = image
-//   }
-
-//   if (imageURl) {
-//     imageInner.style.backgroundImage = `url(${imageURl})`
-//   }
-
-//   imageFrame.appendChild(imageInner)
-//   card.appendChild(imageFrame)
-
-//   const container = document.querySelector('.C_DMemesTeasers')
-// }
+var token = 'patG14HvKR4J0SmjX.fb19163575d0f3e81d543098904dd27c220bfb4d0881cccb39ba12ebf73cf0c4';
+airtable__WEBPACK_IMPORTED_MODULE_0___default().configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: token
+});
+var base = airtable__WEBPACK_IMPORTED_MODULE_0___default().base('app0vbth4eKDLaq0B');
+getMemes().then(function (content) {
+  updateMemes(content);
+});
+function getMemes() {
+  return new Promise(function (resolve) {
+    var content = [];
+    base('C_DMemes').select({
+      maxRecords: 50
+    }).firstPage().then(function (result) {
+      result.forEach(function (record) {
+        content.push({
+          id: record.id,
+          image: record.fields['Images']
+        });
+      });
+      resolve(content);
+    });
+  });
+}
+function updateMemes(content) {
+  content.forEach(function (stroke) {
+    createMemeCard(stroke);
+  });
+}
+function createMemeCard(stroke) {
+  var image = stroke.image;
+  var card = document.createElement('div');
+  card.classList.add('M_DMemesTeaser');
+  var imageFrame = document.createElement('div');
+  imageFrame.classList.add('M_DMemesTeaserImage');
+  var imageInner = document.createElement('div');
+  imageInner.classList.add('M_DMemesTeaserImageInner');
+  var imageURl = '';
+  if (Array.isArray(image) && image.length > 0) {
+    imageURl = image[0].url;
+  } else if (typeof image === 'string') {
+    imageURl = image;
+  }
+  if (imageURl) {
+    imageInner.style.backgroundImage = "url(".concat(imageURl, ")");
+  }
+  imageFrame.appendChild(imageInner);
+  card.appendChild(imageFrame);
+  var container = document.querySelector('.C_DMemesTeasers');
+  if (container) {
+    container.appendChild(card);
+  }
+}
 })();
 
 /******/ })()
